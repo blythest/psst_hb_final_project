925acfde621842bd7f8b5758de13bf83bbc1f91d
Blythe Sheldon <blythe.sheldon@gmail.com>
2014-04-30 13:27:15 -0700

    Clean up and run server in background.

--- scan.sh
+++ scan.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# now, just type command lines.
-
 echo 'Install all the things!'
-ls
-cat /etc/passwd
+
 
 virtualenv virtualenv
 source virtualenv/bin/activate
 pip install -r requirements.txt
-python views.py
-open http://localhost:5000
+python views.py &
+open 'http://localhost:5000'
